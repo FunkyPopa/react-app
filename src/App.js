a73194b8e58bc6a272be09102b42156685260ac8
FunkyPopa <andriybuno370@gmail.com>
2022-10-12 13:48:40 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {Route, Routes} from "react-router-dom";
-import {MainLayout} from "./layout/MainLayout";
+
+import {MainLayout} from "./layout";
 import {PostsPage, UsersPage} from "./pages";
 
 
